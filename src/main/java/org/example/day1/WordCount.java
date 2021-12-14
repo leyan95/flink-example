790288e07d793a0eafb84edbeb7a8a5ff5e28379
leyan95 <18231926271@163.com>
2021-12-14 22:15:42 +0800

    scala word count.

--- src/main/java/org/example/day1/WordCount.java
+++ src/main/java/org/example/day1/WordCount.java
@@ -1,12 +1,11 @@
 package org.example.day1;
 
 import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.api.common.typeinfo.Types;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-
-import java.util.Arrays;
 
 /**
  * @author 吴建川
@@ -18,15 +17,18 @@ public class WordCount {
     public static void main(String[] args) throws Exception {
         StreamExecutionEnvironment executionEnvironment = StreamExecutionEnvironment.getExecutionEnvironment();
         DataStream<String> lines = executionEnvironment.socketTextStream("localhost", 8888);
-        final DataStream<Tuple2<String, Integer>> countStream = lines.flatMap(
-                (FlatMapFunction<String, Tuple2<String, Integer>>) (line, collector) -> Arrays.asList(line.split(" "))
-                        .forEach(word -> {
-                            Tuple2<String, Integer> tp = Tuple2.of(word, 1);
-                            collector.collect(tp);
-                        })
-        ).returns(Types.TUPLE(Types.STRING, Types.INT));
-        DataStream<Tuple2<String, Integer>> sum = countStream.keyBy(0).sum(1);
-        sum.print().setParallelism(1);
+
+        DataStream<String> words = lines.flatMap((FlatMapFunction<String, String>) (line, collector) -> {
+            String[] words1 = line.split(" ");
+            for (String word : words1) {
+                collector.collect(word);
+            }
+        }).returns(Types.STRING);
+
+        DataStream<Tuple2<String, Integer>> wordAndOne = words.map((MapFunction<String, Tuple2<String, Integer>>) word -> Tuple2.of(word, 1)).returns(Types.TUPLE(Types.STRING, Types.INT));
+
+        DataStream<Tuple2<String, Integer>> sum = wordAndOne.keyBy(tp -> tp.f0).sum(1);
+        sum.print();
         executionEnvironment.execute("StreamWordCount");
     }
 }
