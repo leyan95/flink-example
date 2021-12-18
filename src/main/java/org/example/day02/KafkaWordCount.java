45802624c87fbbde16df786a3472aa18f3debc75
Sword <leyansorosame@gmail.com>
2021-12-18 17:01:39 +0800

    kafka word count.

--- src/main/java/org/example/day02/KafkaWordCount.java
+++ src/main/java/org/example/day02/KafkaWordCount.java
@@ -1,7 +1,11 @@
 package org.example.day02;
 
 import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.api.common.serialization.SimpleStringSchema;
+import org.apache.flink.api.common.typeinfo.Types;
+import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.connector.kafka.source.KafkaSource;
 import org.apache.flink.connector.kafka.source.enumerator.initializer.OffsetsInitializer;
 import org.apache.flink.streaming.api.datastream.DataStream;
@@ -12,7 +16,7 @@ import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
  * @author wujianchuan 2021/12/18
  * @version 1.0
  */
-public class KafkaPrint {
+public class KafkaWordCount {
 
     public static void main(String[] args) throws Exception {
         StreamExecutionEnvironment environment = StreamExecutionEnvironment.getExecutionEnvironment();
@@ -25,9 +29,19 @@ public class KafkaPrint {
                 .setValueOnlyDeserializer(new SimpleStringSchema())
                 .build();
 
-        DataStream<String> lines = environment.fromSource(source, WatermarkStrategy.noWatermarks(), "Kafka Source");
+        DataStream<String> words = environment.fromSource(source, WatermarkStrategy.noWatermarks(), "Kafka Source")
+                .flatMap((FlatMapFunction<String, String>) (line, collector) -> {
+                    String[] lineWords = line.split(" ");
+                    for (String word : lineWords) {
+                        collector.collect(word);
+                    }
+                }).returns(Types.STRING);
 
-        lines.print();
+        DataStream<Tuple2<String, Integer>> wordAndOne = words.map((MapFunction<String, Tuple2<String, Integer>>) word -> Tuple2.of(word, 1)).returns(Types.TUPLE(Types.STRING, Types.INT));
+
+        DataStream<Tuple2<String, Integer>> sum = wordAndOne.keyBy(tp -> tp.f0).sum(1);
+
+        sum.print();
 
         environment.execute("KafkaSource");
     }
